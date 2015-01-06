adddb617cf01267803fd4be8591b6cc4eda56173
ksss <co000ri@gmail.com>
2015-01-07 08:39:53 +0900

    fix extconf message

--- ext/epoll/extconf.rb
+++ ext/epoll/extconf.rb
@@ -2,7 +2,7 @@ require 'mkmf'
 
 if !have_header("sys/epoll.h")
   puts "[31m*** complie error: gem 'epoll' must be installed <sys/epoll.h>. ***[m"
-  puts "[31m*** you can require 'io/epoll'. But, you can not use IO::Epoll APIs. ***[m"
+  puts "[31m*** you can require 'epoll'. But, you can not use Epoll APIs. ***[m"
 end
 have_func("epoll_create1", "sys/epoll.h")
 create_makefile('epoll/core')
